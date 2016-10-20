f383395b388ad0804cc160ac38f1c0d441787321
tshemsedinov <timur.shemsedinov@gmail.com>
2016-10-21 02:08:41 +0300

    Added jshint and fixed small bug

---
+++ JavaScript/framework.js
@@ -0,0 +1,27 @@
+// Example showing us how the framework creates an environment (sandbox) for
+// appication runtime, load an application code and passes a sandbox into app
+// as a global context and receives exported application interface
+
+// The framework can require core libraries
+global.api = {};
+api.fs = require('fs');
+api.vm = require('vm');
+
+// Create a hash and turn it into the sandboxed context which will be
+// the global context of an application
+let context = { module: {}, console: console };
+context.global = context;
+let sandbox = api.vm.createContext(context);
+
+// Read an application source code from the file
+let fileName = './application.js';
+api.fs.readFile(fileName, (err, src) => {
+  // We need to handle errors here
+
+  // Run an application in sandboxed context
+  let script = api.vm.createScript(src, fileName);
+  script.runInNewContext(sandbox);
+
+  // We can access a link to exported interface from sandbox.module.exports
+  // to execute, save to the cache, print to console, etc.
+});
