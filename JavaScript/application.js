2b25aa394ae5fd4224709dbcb495690bca8a2fc6
Ruslan <rv.volovik88@gmail.com>
2019-12-31 00:33:51 +0200

    Tasks in repository Sandboxes

--- JavaScript/application.js
+++ JavaScript/application.js
@@ -4,16 +4,32 @@
 // of a sample application to be executed in the sandboxed context by
 // another pice of code from `framework.js`. Read README.md for tasks.
 
-const fs = require('fs');
-const net = require('net');
+const o = new Map ([
+  [ ((field) => field)('field'), 'hello' ],
+  [ 'field2', new Object({ field1: 10, field2: '20' }) ],
+  [ (fn => fn)('fn'), new Function('a, b', 'return a + b') ],
+  [ (function arr(name){return name})('arr'), new Array(1,2,3) ],
+  [ ['boolean'], new Boolean(new Set() instanceof Object).valueOf() ],
+  //[ (async (n) => {return n})('async').then(console.log), new Promise(resolve => resolve()) ]
+])
 
 // Print from the global context of application module
-console.log('From application global context');
-console.dir({ fs, net }, { depth: 1 });
-console.dir({ global }, { depth: 1 });
-console.dir({ api }, { depth: 2 });
+const fn = new Function('a,s', 'return a + s');
 
+// require module fs and util
+api.console.log(api.util.inspect(o));
+
+api.console.log('From global context');
+require('fs');
+require('util');
+
+// exported function
 module.exports = () => {
-  // Print from the exported function context
-  console.log('From application exported function');
-};
+  api.timers.setTimeout(() => api.console.log('From exported timeout'), 1000)
+  api.timers.setInterval(() => api.console.log('From exported timeout'), 2000)
+}
+
+// usage of timers
+api.timers.setTimeout(() => {
+  api.console.log('From timer');
+}, 1000)
